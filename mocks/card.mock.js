aef6d76ec6cbe2898be87bc629f4663c3d488ac0
lfnwil <lfnwilliam@gmail.com>
2025-04-08 16:13:00 +0200

    card mocks

---
+++ mocks/card.mock.js
@@ -0,0 +1,52 @@
+const mockCards = [
+  {
+    id: 1,
+    name: "Flame Strike",
+    type: "attack",
+    rarity: "rare",
+    color: "red",
+    image: "https://example.com/images/flame-strike.png",  // Lien vers l'image
+  },
+  {
+    id: 2,
+    name: "Shield Wall",
+    type: "defense",
+    rarity: "common",
+    color: "blue",
+    image: "https://example.com/images/shield-wall.png",
+  },
+  {
+    id: 3,
+    name: "Healing Light",
+    type: "heal",
+    rarity: "epic",
+    color: "green",
+    image: "https://example.com/images/healing-light.png",
+  },
+  {
+    id: 4,
+    name: "Mana Boost",
+    type: "bonus",
+    rarity: "legendary",
+    color: "purple",
+    image: "https://example.com/images/mana-boost.png",
+  },
+  {
+    id: 5,
+    name: "Poison Trap",
+    type: "malus",
+    rarity: "rare",
+    color: "yellow",
+    image: "https://example.com/images/poison-trap.png",
+  },
+  {
+    id: 6,
+    name: "Dragon Breath",
+    type: "special",
+    rarity: "legendary",
+    color: "red",
+    image: "https://example.com/images/dragon-breath.png",
+  }
+];
+
+export default mockCards;
